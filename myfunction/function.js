c50dae3e1b658c8cef1630882d223b80a2ae249d
simone menichetti <simon3menichettui95@gmail.com>
2024-06-17 17:57:53 +0200

    feat:( add style for show input numbers , add funcion for verify numbers Utent and save choise input in new array utent and return to value for control and visualize Right or wrong result

--- myfunction/function.js
+++ myfunction/function.js
@@ -1,8 +1,9 @@
-// Funzione per generare 5 numeri random da 1 a 100
 
+let arrayNumeri = [];
+// Funzione per generare 5 numeri random da 1 a 100
 function generaNumeriCasuali(quantita, max) {
     // creazione di un array per contenere i numeri generati 
-    const arrayNumeri = [];
+    // const arrayNumeri = [];
     // inizio di un ciclo while per riempire l'array dedicato ad i numeri generati
     while (arrayNumeri.length < quantita) {
         // utilizzo di math.floor per la generazione del numero random
@@ -38,4 +39,58 @@ function visualizzatoreNumeri() {
         // console.log(numeriDiv);
         console.log("Gli elementi creati nel div numeri sono",p);
     }
+
+    // far partire un timer di 30 secondi utilizzando set timeout / 30000        millisecondi equivale a 30 secondi
+
+
+setTimeout(
+    function() {
+        // creazione riferimento per nascondere i numeri al termine dei 30 secondi
+        nascondiNumeri(numeriDiv);
+        mostraInputNumeri();
+    }, 1000); 
+
+    // utilizzo funzione di rimozione innerhtml per nascondere i numeri 
+    function nascondiNumeri(numeriDiv) {
+        numeriDiv.innerHTML = ''; 
+    }
+    
+  
+}
+
+
+// interazione utente inserimento dei 5 numeri visti precedentemente in pagina uno alla volta
+
+
+
+// Funzione per mostrare l'input per i numeri dell'utente
+function mostraInputNumeri() {
+    document.getElementById('inputNumeri').style.display = 'block';
+}
+
+// Funzione per verificare i numeri inseriti dall'utente
+
+function verificaNumeri() {
+    // creazione riferimento input html 
+    const numeriUtenteInput = document.getElementById('numeriUtente').value;
+    // utilizzo di split per dividere una stringa in un array di sottostringhe
+    const numeriUtenteArray = numeriUtenteInput.split(',');
+    const numeriUtente = numeriUtenteArray.map(
+        function(numeroUtente) {
+            // utilizzo della proprietà trim per rimuovere gli spazi nell interazione utente 
+            return parseInt(numeroUtente.trim());
+    
+        }   
+);
+// utilizzo della proprietà filter per filtrare gli elementi di un array in base a un criterio definito e restituisce solo quelli che corrispondono a tale criterio. in questo caso i numeri indovinati 
+    const numeriIndovinati = numeriUtente.filter(
+        function(numeroUtente) {
+            return arrayNumeri.includes(numeroUtente);
+        }
+    );
+
+    const risultato = 'il totale dei numeri indovinati è ' + numeriIndovinati.length
+    const numeriGiusti = ' i numeri indovinati sono:' + numeriIndovinati.join(', ');
+    document.querySelector('.risultato').textContent = risultato;
+    document.querySelector(".numeri-indovinati").textContent= numeriGiusti;
 }
--- myjavascript/mainjavascript.js
+++ myjavascript/mainjavascript.js
@@ -1,11 +1,11 @@
 // Visualizzare in pagina 5 numeri casuali.
 const numeriVisualizzati= visualizzatoreNumeri();
 
-// Dopo aver visualizzato in pagina i 5 numeri casuali
-// far partire un timer di 30 secondi
 
-// al termine dei 30 secondi i numeri dovranno scomparire
 
 // interazione utente inserimento dei 5 numeri visti precedentemente in pagina uno alla volta
+
+   // Aggiungi un listener per il click del pulsante Invia
+document.getElementById('inviaButton').addEventListener('click', verificaNumeri);
 
 // Dopo che sono stati inseriti i 5 numeri dall'utente, il software dichiara quanti e quali dei numeri da indovinare sono stati individuati.
